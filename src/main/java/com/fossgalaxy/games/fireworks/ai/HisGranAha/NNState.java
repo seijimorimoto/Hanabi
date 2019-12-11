a2d743f5cb336afdab9cdf4074f46f5646667986
Angel Seiji Morimoto Burgos <angel_seiji@hotmail.com>
2019-12-11 07:22:15 +0000

    - Updated the NNState to include the new selected features for representing the game state. - Updated the NeuralNetwork file to include the configuration for building the simple (final) neural network model using DL4j, importing it and saving it. - Included models generated through Keras and DL4J on data using the new format for state representation

--- src/main/java/com/fossgalaxy/games/fireworks/ai/HisGranAha/NNState.java
+++ src/main/java/com/fossgalaxy/games/fireworks/ai/HisGranAha/NNState.java
@@ -1,81 +1,62 @@
 package com.fossgalaxy.games.fireworks.ai.HisGranAha;
 
-import com.fossgalaxy.games.fireworks.state.Card;
 import com.fossgalaxy.games.fireworks.state.CardColour;
-import com.fossgalaxy.games.fireworks.state.Deck;
 import com.fossgalaxy.games.fireworks.state.GameState;
 
 import java.util.Arrays;
-import java.util.Collection;
 
 public class NNState {
+    public static final int MAX_SCORE = 25;
     private static final int MAX_PLAYER_COUNT = 5;
     private static final int MAX_INFO_TOKENS = 8;
-    private static final int MAX_CARD_IN_PILE = 5;
+    private static final int MAX_CARDS_IN_PILE = 5;
     private static final int MAX_LIVES = 3;
-    private static final int MAX_SCORE = 25;
-    private static final int MAX_NUM_CARD_1 = 3;
-    private static final int MAX_NUM_CARD_5 = 1;
-    private static final int MAX_NUM_CARD_REST = 2;
-    private static final int NUM_FEATURES = 64;
+    private static final int NUM_FEATURES = 18;
 
-    int turnNumber; // It is not currently being used for training.
     int playerCount;
     int information;
-    int cardColorRed;
-    int cardColorBlue;
-    int cardColorGreen;
-    int cardColorOrange;
-    int cardColorWhite;
     int nextAgentOffset;
     int lives;
+    int[] cardValuesCounts;
+    int[] cardColourCounts;
     int score;
-    int[][] deckCards; // It is not being used to determine if two NNStates are equal.
-    int[][] discardCards;
-    double stateValue; // It is just used if part of a data gathering process.
 
     public NNState(GameState gameState, int nextAgentOffset) {
-        this.turnNumber = gameState.getTurnNumber();
         this.playerCount = gameState.getPlayerCount();
         this.information = gameState.getInfomation();
-        this.cardColorRed = gameState.getTableValue(CardColour.RED);
-        this.cardColorBlue = gameState.getTableValue(CardColour.BLUE);
-        this.cardColorGreen = gameState.getTableValue(CardColour.GREEN);
-        this.cardColorOrange = gameState.getTableValue(CardColour.ORANGE);
-        this.cardColorWhite = gameState.getTableValue(CardColour.WHITE);
         this.nextAgentOffset = nextAgentOffset;
         this.lives = gameState.getLives();
+        setCardCounts(gameState);
         this.score = gameState.getScore();
-        this.deckCards = new int[5][5];
-        setDeckCards(gameState.getDeck());
-        this.discardCards = new int[5][5];
-        setDiscardCards(gameState.getDiscards());
     }
 
-    public NNState(GameState gameState, int nextAgentOffset, double stateValue) {
-        this(gameState, nextAgentOffset);
-        this.stateValue = stateValue;
-    }
-
-        public void setDeckCards(Deck deck) {
-        setCardsOnMatrix(deck.toList(), this.deckCards);
-    }
-
-    public void setDiscardCards(Collection<Card> cards) {
-        setCardsOnMatrix(cards, this.discardCards);
-    }
-
-    public void setCardsOnMatrix(Collection<Card> cards, int[][] matrix) {
-        int i = 0;
-        for (Card card : cards) {
-            switch (card.colour) {
-                case RED: { i = 0; break; }
-                case BLUE: { i = 1; break; }
-                case GREEN: { i = 2; break; }
-                case ORANGE: { i = 3; break; }
-                case WHITE: { i = 4; break; }
+    public void setCardCounts(GameState state) {
+        CardColour[] colours = { CardColour.RED, CardColour.BLUE, CardColour.GREEN, CardColour.ORANGE, CardColour.WHITE };
+        this.cardColourCounts = new int[5];
+        this.cardValuesCounts = new int[5];
+        for (int i = 0; i < colours.length; i++) {
+            int cardValue = state.getTableValue(colours[i]);
+            this.cardColourCounts[i] = cardValue;
+            for (int j = 0; j < cardValue; j++) {
+                this.cardValuesCounts[j]++;
             }
-            matrix[i][card.value - 1]++;
+        }
+    }
+
+    public double[] getNormalizedFlattenedRepresentation() {
+        double[] representation = new double[NUM_FEATURES];
+        representation[0] = this.playerCount / ((double) MAX_PLAYER_COUNT);
+        representation[1] = this.information / ((double) MAX_INFO_TOKENS);
+        representation[2] = this.lives / ((double) MAX_LIVES);
+        setNormalizedCardCountInRepresentation(3, cardValuesCounts, representation);
+        setNormalizedCardCountInRepresentation(8, cardColourCounts, representation);
+        representation[13 + nextAgentOffset] = 1;
+        return representation;
+    }
+
+    public void setNormalizedCardCountInRepresentation(int startIndex, int[] collection, double[] representation) {
+        for (int i = 0; i < collection.length; i++) {
+            representation[startIndex + i] = collection[i] / ((double) MAX_CARDS_IN_PILE);
         }
     }
 
@@ -83,57 +64,11 @@ public class NNState {
     public String toString() {
         return playerCount + "," +
                 information + "," +
-                cardColorRed + "," +
-                cardColorBlue + "," +
-                cardColorGreen + "," +
-                cardColorOrange + "," +
-                cardColorWhite + "," +
                 nextAgentOffset + "," +
                 lives + "," +
-                score + "," +
-                matrixToString(deckCards) +
-                matrixToString(discardCards);
-    }
-
-    public String matrixToString(int[][] matrix) {
-        String matrixString = "";
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrixString += Integer.toString(matrix[i][j]) + ",";
-            }
-        }
-        return matrixString;
-    }
-
-    public double[] getNormalizedFlattenedRepresentation() {
-        double[] representation = new double[NUM_FEATURES];
-        representation[0] = this.playerCount / ((double) MAX_PLAYER_COUNT);
-        representation[1] = this.information / ((double) MAX_INFO_TOKENS);
-        representation[2] = this.cardColorRed / ((double) MAX_CARD_IN_PILE);
-        representation[3] = this.cardColorBlue / ((double) MAX_CARD_IN_PILE);
-        representation[4] = this.cardColorGreen / ((double) MAX_CARD_IN_PILE);
-        representation[5] = this.cardColorOrange / ((double) MAX_CARD_IN_PILE);
-        representation[6] = this.cardColorWhite / ((double) MAX_CARD_IN_PILE);
-        representation[7] = this.lives / ((double) MAX_LIVES);
-        representation[8] = this.score / ((double) MAX_SCORE);
-        setNormalizedCardCountInRepresentation(9, this.deckCards, representation);
-        setNormalizedCardCountInRepresentation(34, this.discardCards, representation);
-        representation[59 + nextAgentOffset] = 1;
-        return representation;
-    }
-
-    public void setNormalizedCardCountInRepresentation(int startIndex, int[][] collection, double[] representation) {
-        for (int i = 0; i < collection.length; i++) {
-            for (int j = 0; j < collection[i].length; j++) {
-                int reprIndex = startIndex + collection[i].length * i + j;
-                if (j == 0)
-                    representation[reprIndex] = collection[i][j] / ((double) MAX_NUM_CARD_1);
-                else if (j == collection[i].length - 1)
-                    representation[reprIndex] = collection[i][j] / ((double) MAX_NUM_CARD_5);
-                else
-                    representation[reprIndex] = ((double) collection[i][j] / MAX_NUM_CARD_REST);
-            }
-        }
+                String.join(",", Arrays.stream(cardValuesCounts).mapToObj(String::valueOf).toArray(String[]::new)) + "," +
+                String.join(",", Arrays.stream(cardColourCounts).mapToObj(String::valueOf).toArray(String[]::new)) + "," +
+                score + ",";
     }
 
     @Override
@@ -145,29 +80,19 @@ public class NNState {
         if (getClass() != obj.getClass())
             return false;
         NNState other = (NNState) obj;
-        if (turnNumber != other.turnNumber)
-            return false;
         if (playerCount != other.playerCount)
             return false;
         if (information != other.information)
-            return false;
-        if (cardColorRed != other.cardColorRed)
-            return false;
-        if (cardColorBlue != other.cardColorBlue)
-            return false;
-        if (cardColorGreen != other.cardColorGreen)
-            return false;
-        if (cardColorOrange != other.cardColorOrange)
-            return false;
-        if (cardColorWhite != other.cardColorWhite)
             return false;
         if (nextAgentOffset != other.nextAgentOffset)
             return false;
         if (lives != other.lives)
             return false;
-        if (score != other.score)
+        if (!Arrays.equals(cardValuesCounts, other.cardValuesCounts))
             return false;
-        if (!Arrays.deepEquals(discardCards, other.discardCards))
+        if (!Arrays.equals(cardColourCounts, other.cardColourCounts))
+            return false;
+        if (score != other.score)
             return false;
         return true;
     }
@@ -176,18 +101,13 @@ public class NNState {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + Integer.hashCode(turnNumber);
         result = prime * result + Integer.hashCode(playerCount);
         result = prime * result + Integer.hashCode(information);
-        result = prime * result + Integer.hashCode(cardColorRed);
-        result = prime * result + Integer.hashCode(cardColorBlue);
-        result = prime * result + Integer.hashCode(cardColorGreen);
-        result = prime * result + Integer.hashCode(cardColorOrange);
-        result = prime * result + Integer.hashCode(cardColorWhite);
         result = prime * result + Integer.hashCode(nextAgentOffset);
         result = prime * result + Integer.hashCode(lives);
+        result = prime * result + Arrays.hashCode(cardValuesCounts);
+        result = prime * result + Arrays.hashCode(cardColourCounts);
         result = prime * result + Integer.hashCode(score);
-        result = prime * result + Arrays.deepHashCode(discardCards);
         return result;
     }
 }
--- src/main/java/com/fossgalaxy/games/fireworks/ai/HisGranAha/NeuralNetwork.java
+++ src/main/java/com/fossgalaxy/games/fireworks/ai/HisGranAha/NeuralNetwork.java
@@ -5,15 +5,14 @@ import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.deeplearning4j.datasets.datavec.RecordReaderMultiDataSetIterator;
 import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
-import org.deeplearning4j.nn.conf.InputPreProcessor;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.layers.*;
-import org.deeplearning4j.nn.conf.preprocessor.CnnToFeedForwardPreProcessor;
-import org.deeplearning4j.nn.conf.preprocessor.FeedForwardToCnnPreProcessor;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
 import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.iterator.MultiDataSetIterator;
@@ -23,8 +22,6 @@ import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class NeuralNetwork {
@@ -49,42 +46,25 @@ public class NeuralNetwork {
     public NeuralNetworkOutput predict(double[] stateFeatures) {
         INDArray inputsAsINDArray = Nd4j.create(stateFeatures, new int[]{stateFeatures.length});
         inputsAsINDArray = Nd4j.expandDims(inputsAsINDArray, 0);
-        inputsAsINDArray = Nd4j.expandDims(inputsAsINDArray, 0);
         INDArray[] outputs = this.model.output(inputsAsINDArray);
         double[] policy = outputs[0].toDoubleVector();
-        double value = outputs[1].toDoubleVector()[0];
+        double value = outputs[1].toDoubleVector()[0] * NNState.MAX_SCORE;
         return new NeuralNetworkOutput(policy, value);
     }
 
     public void compile() {
-        Map<String, InputPreProcessor> preProcessorMap = new HashMap<>();
-        preProcessorMap.put("Conv1D_1", new FeedForwardToCnnPreProcessor(64, 1, 1));
-        preProcessorMap.put("Dense1", new CnnToFeedForwardPreProcessor(60, 512, 1));
-
         ComputationGraphConfiguration.GraphBuilder confBuilder = new NeuralNetConfiguration.Builder()
                 .updater(new Adam(0.001))
                 .graphBuilder()
                 .addInputs("input")
-                .addLayer("Conv1D_1", new Convolution1D.Builder().kernelSize(64).stride(3).nIn(1).nOut(64).hasBias(false).activation(Activation.RELU).build(), "input")
-                .addLayer("BatchNorm1", new BatchNormalization.Builder().nIn(64).nOut(64).build(), "Conv1D_1")
-                .addLayer("Conv1D_2", new Convolution1D.Builder().kernelSize(128).stride(3).nIn(64).nOut(64).hasBias(false).activation(Activation.RELU).build(), "BatchNorm1")
-                .addLayer("BatchNorm2", new BatchNormalization.Builder().nIn(64).nOut(64).build(), "Conv1D_2")
-                .addLayer("Conv1D_3", new Convolution1D.Builder().kernelSize(256).stride(3).nIn(64).nOut(64).hasBias(false).activation(Activation.RELU).build(), "BatchNorm2")
-                .addLayer("BatchNorm3", new BatchNormalization.Builder().nIn(64).nOut(64).build(), "Conv1D_3")
-                .addLayer("Conv1D_4", new Convolution1D.Builder().kernelSize(512).stride(3).nIn(64).nOut(64).hasBias(false).activation(Activation.RELU).build(), "BatchNorm3")
-                .addLayer("BatchNorm4", new BatchNormalization.Builder().nIn(64).nOut(64).build(), "Conv1D_4")
-                .addLayer("Dense1", new DenseLayer.Builder().nIn(32768).nOut(1024).hasBias(false).activation(Activation.RELU).build(), "BatchNorm4")
-                .addLayer("BatchNormD1", new BatchNormalization.Builder().nIn(1024).nOut(1024).build(), "Dense1")
-                .addLayer("Drop1", new DropoutLayer.Builder().dropOut(0.7).nIn(1024).nOut(1024).build(), "BatchNormD1")
-                .addLayer("Dense2", new DenseLayer.Builder().nIn(1024).nOut(512).hasBias(false).activation(Activation.RELU).build(), "Drop1")
-                .addLayer("BatchNormD2", new BatchNormalization.Builder().nIn(512).nOut(512).build(), "Dense2")
-                .addLayer("Drop2", new DropoutLayer.Builder().dropOut(0.7).nIn(512).nOut(512).build(), "BatchNormD2")
-                .addLayer("Pi", new OutputLayer.Builder().nIn(512).nOut(60).activation(Activation.SOFTMAX).lossFunction(LossFunctions.LossFunction.MCXENT).build(), "Drop2")
-                .addLayer("V", new OutputLayer.Builder().nIn(512).nOut(1).activation(Activation.RELU).lossFunction(LossFunctions.LossFunction.MSE).build(), "Drop2")
+                .addLayer("Dense1", new DenseLayer.Builder().nIn(18).nOut(32).activation(Activation.RELU).build(), "input")
+                .addLayer("Dense2", new DenseLayer.Builder().nIn(32).nOut(16).activation(Activation.RELU).build(), "Dense1")
+                .addLayer("Pi", new OutputLayer.Builder().nIn(16).nOut(60).activation(Activation.SOFTMAX).lossFunction(LossFunctions.LossFunction.MCXENT).build(), "Dense2")
+                .addLayer("V", new OutputLayer.Builder().nIn(16).nOut(1).activation(Activation.SIGMOID).lossFunction(LossFunctions.LossFunction.MSE).build(), "Dense2")
                 .setOutputs("Pi", "V");
-        confBuilder.setInputPreProcessors(preProcessorMap);
-
         this.model = new ComputationGraph(confBuilder.build());
+        this.model.init();
+        this.model.setListeners(new ScoreIterationListener(1000));
     }
 
     public void importKerasModel(String modelConfigPath, String modelWeightsPath)
@@ -92,20 +72,27 @@ public class NeuralNetwork {
         this.model = KerasModelImport.importKerasModelAndWeights(modelConfigPath, modelWeightsPath);
     }
 
-    public void setTrainData(String trainFeaturesPath, String trainLabelsPath, int numLinesSkip, int batchSize)
+    public void importDL4JModel(String modelPath, boolean loadForRetraining) throws IOException
+    {
+        this.model = ComputationGraph.load(new File(modelPath), loadForRetraining);
+    }
+
+    public void saveModel(String savePath, boolean canBeRetrained) throws IOException
+    {
+        this.model.save(new File(savePath), canBeRetrained);
+    }
+
+    public void setTrainData(String trainDataSetPath, int numLinesSkip, int batchSize)
             throws InterruptedException, IOException {
         String delimiter = ",";
-        RecordReader trainFeaturesReader = new CSVRecordReader(numLinesSkip, delimiter);
-        trainFeaturesReader.initialize(new FileSplit(new File(trainFeaturesPath)));
-        RecordReader trainLabelsReader = new CSVRecordReader(numLinesSkip, delimiter);
-        trainLabelsReader.initialize(new FileSplit(new File(trainLabelsPath)));
+        RecordReader trainReader = new CSVRecordReader(numLinesSkip, delimiter);
+        trainReader.initialize(new FileSplit(new File(trainDataSetPath)));
 
         this.modelIterator = new RecordReaderMultiDataSetIterator.Builder(batchSize)
-                .addReader("trainFeatures", trainFeaturesReader)
-                .addReader("trainLabels", trainLabelsReader)
-                .addInput("trainFeatures")
-                .addOutput("trainLabels", 0, 59)
-                .addOutput("trainLabels", 60, 60)
+                .addReader("trainReader", trainReader)
+                .addInput("trainReader", 0, 17)
+                .addOutput("trainReader", 19, 78)
+                .addOutput("trainReader", 18, 18)
                 .build();
     }
 
